3ebdaff212d667fdba1f985a7cf9044413b8dd2b
alexssandrodamiao <alexssandro.damiao@acertpix.com.br>
2025-10-20 20:35:08 -0300

    resolução do desafio 91

--- src/desafio_91.py
+++ src/desafio_91.py
@@ -1,3 +1,24 @@
 from random import randint
+from operator import itemgetter
+from time import sleep
 
-print(randint(0,6))
+print('Iniciando o game')
+print('')
+sorteio = dict()
+ranking = []
+
+for x in range(1,5):
+    sorteio[f'Jogador{x}'] = randint(1,6)
+    sleep(2)
+
+print('Valores sorteados')
+
+for k, v in sorteio.items():
+    print(f'{k} tirou {v}')
+
+print('+=' * 30)
+print('== Ranking dos jogadores ==')
+ranking = sorted(sorteio.items(), key=itemgetter(1), reverse=True)
+
+for i, v in enumerate(ranking):
+    print(f'{i+1}° lugar: {v[0]} com {v[1]}.')
